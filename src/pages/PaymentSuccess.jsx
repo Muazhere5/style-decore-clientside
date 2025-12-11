24090a185e68425945c25be3296ceae51732208f
Muazhere5 <muazctg07@gmail.com>
2025-12-11 23:24:03 +0600

    SuccessPayement

---
+++ src/pages/PaymentSuccess.jsx
@@ -0,0 +1,12 @@
+const PaymentSuccess = () => {
+  return (
+    <div className="text-center py-20">
+      <h1 className="text-4xl font-bold text-green-600">
+        Payment Successful!
+      </h1>
+      <p className="mt-4">Thank you for your booking. Your order is confirmed.</p>
+    </div>
+  );
+};
+
+export default PaymentSuccess;
